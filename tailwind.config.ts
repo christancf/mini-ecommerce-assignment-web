b453fc10d7a41f74824c90a60d9fadfe4dcc2bcf
Christan Chandra <christancf.dev@gmail.com>
2024-08-10 22:09:31 +0530

    implement products page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,30 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      width: {
+        "58px": "58px",
+      },
+      fontSize: {
+        "19px": "19px",
+      },
+      lineHeight: {
+        "25.65px": "25.65px",
+      },
+      borderRadius: {
+        "10px": "10px",
+      },
+      colors: {
+        "gray-250": "#f7f7f7",
+        "gray-350": "#969191",
+        "blue-custom": "#001eb9",
+        "text-primary": "#162427",
+        "green-online": "#3df265",
+      },
+      fontFamily: {
+        "satoshi-400": ["Satoshi-Regular", "sans-serif"],
+        "satoshi-500": ["Satoshi-Medium", "sans-serif"],
+        "satoshi-700": ["Satoshi-Bold", "sans-serif"],
+        "satoshi-900": ["Satoshi-Black", "sans-serif"],
       },
     },
   },
